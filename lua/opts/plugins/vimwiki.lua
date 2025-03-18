6cbdc3e292ebb412b041818c9a0765bba184668c
KanielDasper <dkdanielkasper@gmail.com>
2025-03-18 21:07:20 +0100

    Remove python script, just use lua lol

---
+++ lua/opts/plugins/vimwiki.lua
@@ -0,0 +1,38 @@
+return {
+	"vimwiki/vimwiki",
+	init = function()
+		vim.g.vimwiki_list = {
+			{
+				path = "~/Documents/Vimwiki/",
+				syntax = "markdown",
+				ext = ".md",
+				auto_toc = 1,
+				index = "index",
+				auto_diary_index = 1,
+			},
+		}
+		vim.g.vimwiki_use_calendar = 1
+
+		-- Autocmd for diary template
+		vim.api.nvim_create_autocmd("BufNewFile", {
+			pattern = "*/diary/*.md",
+			callback = function()
+				local bufnr = vim.api.nvim_get_current_buf()
+				if vim.api.nvim_buf_line_count(bufnr) == 1 and vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] == "" then
+					local date = os.date("%Y-%m-%d")
+					local template = {
+						"# " .. date,
+						"Back to [Diary Index](Diary)",
+						"",
+						"## Notes",
+						"",
+						"## Todo",
+						"",
+						"* [ ]"
+					}
+					vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, template)
+				end
+			end,
+		})
+	end,
+}
