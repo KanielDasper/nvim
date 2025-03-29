983933efce8c7209349ffa3440a63f030ff1f34c
KanielDasper <dkdanielkasper@gmail.com>
2025-03-30 00:07:39 +0100

    Add AI and lualine tweak

---
+++ lua/opts/plugins/ai_codeium.lua
@@ -0,0 +1,22 @@
+return {
+	"Exafunction/codeium.vim",
+	config = function()
+		vim.g.codeium_enabled = false
+		-- Toggle AI completion
+		vim.api.nvim_create_user_command("CodeiumToggle", function()
+			vim.g.codeium_enabled = not vim.g.codeium_enabled
+		end, {})
+		vim.keymap.set({ "n", "i" }, "<C-c>", "<cmd>CodeiumToggle<CR>", { silent = true })
+
+		-- Cycle and accept ai completion
+		vim.keymap.set("i", "<C-f>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<C-k>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<C-j>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true, silent = true })
+	end,
+}
